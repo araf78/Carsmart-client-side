1aae2f35cff5fcb805970264fdf00e5b9d8ac02a
unknown <alarafath71@gmail.com>
2022-05-02 16:13:05 +0600

    login and sign out toggle

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { getAuth, signOut } from "@firebase/auth";
+import app from "../../../firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const auth = getAuth(app);
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+  console.log(user)
   return (
     <nav className='header d-flex justify-content-around  align-items-center'>
       <h2 className="fs-1 fw-bold">
@@ -12,7 +19,12 @@ const Header = () => {
         <Link to="/">Home</Link>
         <Link to="/services">Services</Link>
         <Link to="/blogs">Blogs</Link>
+        {
+          user?.uid ? 
+          <button className="btn btn-outline-dark fw-bolder" onClick={()=> signOut(auth)}>Sign out</button>
+          :
         <Link to="/login">Login</Link>
+        }
       </div>
     </nav>
   );
